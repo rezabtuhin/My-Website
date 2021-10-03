4df2afe9e04af705ae9c5a6c7769108c5ed0a9f3
Rezab Ud Dawla <72731142+rezabtuhin@users.noreply.github.com>
2021-10-03 19:49:45 +0600

    Add files via upload

---
+++ My Website/app.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.short-intro,.find,.onweb,.skills-tag,.skills-head,.contact-def',{delay:200,origin:'left'});
+ScrollReveal().reveal('.section-2 p',{delay:200,origin:'right'});
+ScrollReveal().reveal('.project-box-wrapper,.skill-box-wrapper',{delay:200,origin:'bottom',interval:200});
+ScrollReveal().reveal('.web-box-wrapper,.heading-2,.contact-box',{delay:200,origin:'bottom',interval:200});
+ScrollReveal().reveal('.bar',{delay:200,origin:'top',interval:200});
+ScrollReveal().reveal('.heading-2,.contact-box',{delay:200,origin:'bottom',interval:200});
